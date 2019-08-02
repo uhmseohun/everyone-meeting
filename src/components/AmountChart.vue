6675bdf6b4a146fc188a4eb9c04e9013e9e14123
uhmtoto <uhmtotow@gmail.com>
2019-08-02 19:20:24 +0900

    feat: amount chart component

---
+++ src/components/AmountChart.vue
@@ -0,0 +1,27 @@
+<script>
+import { Doughnut } from 'vue-chartjs'
+
+export default {
+  name: 'AmountChart',
+  extends: Doughnut,
+  data: () => ({
+    chartdata: {
+      labels: ['비활동', '유산소', '근력운동', '근력심화'],
+      datasets: [
+        {
+          backgroundColor: ['#345e96', '#d09e7f', '#a4c2e3', '#fbe6a0'],
+          data: [43, 26, 22, 9]
+        }
+      ]
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false
+    }
+  }),
+
+  mounted () {
+    this.renderChart(this.chartdata, this.options)
+  }
+}
+</script>
