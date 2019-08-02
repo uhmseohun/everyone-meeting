e45666471d4f130d6a219a0b3bca058ff358ff20
uhmtoto <uhmtotow@gmail.com>
2019-08-03 01:29:41 +0900

    feat: add chart components

---
+++ src/components/charts/AchieveRateChart.vue
@@ -0,0 +1,28 @@
+<script>
+import { Bar } from 'vue-chartjs'
+
+export default {
+  name: 'AmountRateChart',
+  extends: Bar,
+  data: () => ({
+    chartdata: {
+      labels: ['비활동', '유산소', '근력운동', '근력심화'],
+      datasets: [
+        {
+          label: '운동 목표 달성량',
+          backgroundColor: ['#df8244', '#f9c044', '#4d72be', '#fbe6a0'],
+          data: [420, 190, 22, 300]
+        }
+      ]
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false
+    }
+  }),
+
+  mounted () {
+    this.renderChart(this.chartdata, this.options)
+  }
+}
+</script>
---
+++ src/components/charts/CalorieChart.vue
@@ -0,0 +1,27 @@
+<script>
+import { Line } from 'vue-chartjs'
+
+export default {
+  name: 'CalorieChart',
+  extends: Line,
+  data: () => ({
+    chartdata: {
+      labels: ['비활동', '유산소', '근력운동', '근력심화'],
+      datasets: [
+        {
+          label: '운동 목표 달성량',
+          data: [420, 190, 22, 300]
+        }
+      ]
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false
+    }
+  }),
+
+  mounted () {
+    this.renderChart(this.chartdata, this.options)
+  }
+}
+</script>
