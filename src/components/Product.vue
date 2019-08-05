1aeafd421a3507bd51b85478ccc2da63f98d7eef
uhmtoto <uhmtotow@gmail.com>
2019-08-05 22:17:41 +0900

    feat: add product component

---
+++ src/components/Product.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+  name: 'Product',
+  props: {
+    product: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<template>
+<div class="product">
+  <img
+    class="product__image"
+    :src="product.image"
+  >
+  <span class="product__name">
+    {{ product. name }}
+  </span>
+</div>
+</template>
+
+<style lang="scss" scoped>
+.product {
+  &__image {
+    width: 6rem;
+    display: block;
+  }
+}
+</style>
