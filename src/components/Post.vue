c8c02fe76c419093ec4effa541b8193690495a5c
uhmtoto <uhmtotow@gmail.com>
2019-08-05 21:11:52 +0900

    feat: post component

---
+++ src/components/Post.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+  name: 'Post',
+  props: {
+    post: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<template>
+<div class="post">
+  <span class="post__title">
+    {{ post.title }}
+  </span>
+  <span class="post__info">
+    {{ post.info }}
+  </span>
+</div>
+</template>
+
+<style lang="scss" scoped>
+.post {
+  &__title {
+    display: block;
+  }
+
+  &__info {
+    font-size: 0.7rem;
+    color: gray;
+  }
+}
+</style>
