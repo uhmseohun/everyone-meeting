3ec52f8d41ed178ff694da2884f105d6efcad5d0
uhmtoto <uhmtotow@gmail.com>
2019-08-05 19:38:23 +0900

    feat: add home screen

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,34 +1,11 @@
 <script>
+import Logo from '@/assets/logo.png'
+
 export default {
   name: 'Home',
   data () {
     return {
-      services: [
-        {
-          title: '종목별 매칭 서비스',
-          to: '/match'
-        },
-        {
-          title: '장소 정보',
-          to: '/place'
-        },
-        {
-          title: '운동량 정보 제공',
-          to: '/amount'
-        },
-        {
-          title: '커뮤니티',
-          to: '/community'
-        },
-        {
-          title: '부가 서비스',
-          to: '/service'
-        },
-        {
-          title: 'COIN',
-          to: '/coin'
-        }
-      ]
+      Logo
     }
   },
   methods: {
@@ -41,32 +18,67 @@ export default {
 
 <template>
 <div class="home">
-  <div
-    @click="push(service.to)"
-    v-for="(service, i) in services"
-    :key="`service-${i}`"
-    class="home__menu"
-  >
-    {{ service.title }}
+  <div class="home__brand">
+    <img :src="Logo" class="home__logo">
+    <div class="home__title">
+      <h1>모두의 모임</h1>
+      <hr>
+      <span class="home__motto">
+        "Not Alone,<br>
+        Let's Together"
+      </span>
+    </div>
   </div>
+  <button
+    @click="push('/join')"
+    class="home__start"
+  >
+    start
+  </button>
 </div>
 </template>
 
 <style lang="scss" scoped>
 .home {
-  &__menu {
+  background: #36D1DC;  /* fallback for old browsers */
+  background: -webkit-linear-gradient(to bottom, #5B86E5, #36D1DC);  /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(to bottom, #5B86E5, #36D1DC); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+  height: calc(100% - 16px); // for navbar, padding height
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  &__brand {
     display: flex;
     justify-content: center;
     align-items: center;
-    padding: 1rem 0;
-    width: 100%;
-    color: gray;
-    border: 3px solid #5B86E5;
-    border-radius: 1rem;
+    color: white;
+    text-align: center;
+  }
 
-    &:not(:first-child) {
-      margin-top: 2rem;
-    }
+  &__motto {
+    flex-direction: column;
+    font-style: oblique;
+  }
+
+  &__start {
+    position: fixed;
+    bottom: 3rem;
+    background-color: rgb(80, 166, 174);
+    font-size: 1.5rem;
+    font-style: oblique;
+    color: white;
+    border: 0;
+    border-radius: 50px;
+    height: 3rem;
+    width: 5rem;
+  }
+
+  &__logo {
+    height: 5rem;
+    margin-right: 10px;
   }
 }
 </style>
