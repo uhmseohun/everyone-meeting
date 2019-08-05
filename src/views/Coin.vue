c6c1684a3b59dffa450d23338417eb7e00a713b8
uhmtoto <uhmtotow@gmail.com>
2019-08-05 22:41:00 +0900

    feat: add background in coin page

--- src/views/Coin.vue
+++ src/views/Coin.vue
@@ -5,7 +5,15 @@ export default {
 </script>
 
 <template>
+<div class="coin">
+</div>
 </template>
 
 <style lang="scss" scoped>
+.coin {
+  min-height: calc(100% - 4.5rem);
+  background: #36D1DC;  /* fallback for old browsers */
+  background: -webkit-linear-gradient(to bottom, #5B86E5, #36D1DC);  /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(to bottom, #5B86E5, #36D1DC); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+}
 </style>
