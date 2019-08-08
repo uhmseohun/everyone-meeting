311b1bd1b7cfccd812e36fdbbdc7cdec6178303d
uhmtoto <uhmtotow@gmail.com>
2019-08-08 17:36:36 +0900

    package: add tooltip dependency

--- src/components/charts/AchieveRateChart.vue
+++ src/components/charts/AchieveRateChart.vue
@@ -1,10 +1,12 @@
 <script>
 import { Bar } from 'vue-chartjs'
+import ChartDataLabels from 'chartjs-plugin-datalabels'
 
 export default {
   name: 'AmountRateChart',
   extends: Bar,
   data: () => ({
+    plugins: [ ChartDataLabels ],
     chartdata: {
       labels: ['비활동', '유산소', '근력운동', '근력심화'],
       datasets: [
@@ -17,7 +19,21 @@ export default {
     },
     options: {
       responsive: true,
-      maintainAspectRatio: false
+      maintainAspectRatio: false,
+      tooltips: {
+        enabled: false
+      },
+      plugins: {
+        datalabels: {
+          color: 'white',
+          font: {
+            size: '15'
+          },
+          formatter (value, context) {
+            return context.dataset.data[context.dataIndex]
+          }
+        }
+      }
     }
   }),
 
